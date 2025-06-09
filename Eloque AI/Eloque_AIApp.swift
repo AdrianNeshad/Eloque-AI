c21f7d099491fcc3d31a2a0e4143569ec03f64dc
Adrian <adrian.neshad1@gmail.com>
2025-06-09 13:59:44 +0200

    Initial Commit

---
+++ Eloque AI/Eloque_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eloque_AIApp.swift
+//  Eloque AI
+//
+//  Created by Adrian Neshad on 2025-06-09.
+//
+
+import SwiftUI
+
+@main
+struct Eloque_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
